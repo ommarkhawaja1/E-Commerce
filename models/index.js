87010aee3672cb754a023e188a982738adabd4ae
ommarkhawaja1 <ommarkhawaja1@gmail.com>
2022-07-20 16:43:17 -0400

    established database models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+	foreignKey: "category_id",
+	onDelete: "SET NULL",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+	foreignKey: "category_id",
+  onDelete: "CASCADE"
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+	through: ProductTag,
+	as: "tags",
+	foreignKey: "product_id",
+	onDelete: "SET NULL",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+	through: ProductTag,
+	as:"products",
+	foreignKey: "tag_id",
+	onDelete: "SET NULL",
+});
 
 module.exports = {
   Product,
